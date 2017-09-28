58bcc01a4c87be0a7fd6c2d2f37d144f32bcde52
M H <morganhartley@gmail.com>
2017-09-28 11:59:27 -0500

    typo checks, map language, spend chart

--- js/chart-data.js
+++ js/chart-data.js
@@ -231,111 +231,25 @@ var voterPrimaryBreakdown = {
 	
 		}
 
-	var pieData = [
-				{
-					value: 300,
-					color: "#8082e4",
-					highlight: "#7376df",
-					label: "Value 1"
-				},
-				{
-					value: 50,
-					color: "#a0a0a0",
-					highlight: "#999999",
-					label: "Value 2"
-				},
-				{
-					value: 100,
-					color:"#dfdfdf",
-					highlight: "#cccccc",
-					label: "Value 3"
-				},
-				{
-					value: 120,
-					color: "#f7f7f7",
-					highlight: "#eeeeee",
-					label: "Value 4"
-				}
+	
+			
+	spendData = {
+		    datasets: [{
+		        data: [15000, 5000, 1000, 10000, 9000,],
+		        label: 'Example Spend',
+		    	backgroundColor: ['#dcdcdc',
+				'#828282',
+				'#f7f7f7',
+				'#b2abd2',
+				'#5e3c99',]
+		    }],
+		    labels: [
+		        'Direct Engagement Ads',
+		        'Demographic/Message Testing',
+		        'Digital Assets',
+		        'Commercial List Aquisition',
+		        'Out of District Fundraising',
+		    	],
+		};
 
-			];
-			
-	var doughnutData = [
-				{
-					value: 300,
-					color: "#8082e4",
-					highlight: "#7376df",
-					label: "Value 1"
-				},
-				{
-					value: 50,
-					color: "#a0a0a0",
-					highlight: "#999999",
-					label: "Value 2"
-				},
-				{
-					value: 100,
-					color:"#dfdfdf",
-					highlight: "#cccccc",
-					label: "Value 3"
-				},
-				{
-					value: 120,
-					color: "#f7f7f7",
-					highlight: "#eeeeee",
-					label: "Value 4"
-				}
-			];
-			
-	var radarData = {
-	    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-	    datasets: [
-	        {
-	            label: "My First dataset",
-	            backgroundColor: "rgba(70,130,180,0.2)",
-	            strokeColor: "rgba(70,130,180,1)",
-	            pointColor: "rgba(70,130,180,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(70,130,180,1)",
-	            data: [65, 59, 90, 81, 56, 55, 40]
-	        },
-	        {
-	            label: "My Second dataset",
-	            backgroundColor : "rgba(197,29,29, 0.6)",
-	            strokeColor : "rgba(197,29,29, 1)",
-	            pointColor : "rgba(197,29,29, 1)",
-	            pointStrokeColor : "#fff",
-	            pointHighlightFill : "#fff",
-	            pointHighlightStroke : "rgba(197,29,29, 1)",
-	            data: [28, 48, 40, 19, 96, 27, 100]
-	        }
-	    ]
-	};
-	var polarData = [
-		    {
-		    	value: 300,
-		    	color: "#8082e4",
-		    	highlight: "#7376df",
-		    	label: "Value 1"
-		    },
-		    {
-		    	value: 140,
-		    	color: "#a0a0a0",
-		    	highlight: "#999999",
-		    	label: "Value 2"
-		    },
-		    {
-		    	value: 220,
-		    	color:"#dfdfdf",
-		    	highlight: "#cccccc",
-		    	label: "Value 3"
-		    },
-		    {
-		    	value: 250,
-		    	color: "#f7f7f7",
-		    	highlight: "#eeeeee",
-		    	label: "Value 4"
-		    }
-		
-	];
 
